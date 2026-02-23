6fd6b5b1305978abc86468370f5adb7ef976c24d
newjiwon <78717933+newjiwon@users.noreply.github.com>
2026-02-23 14:41:25 +0900

    Create baeyul_test04(diamond).c

---
+++ baeyul/baeyul_test04(diamond).c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX 5
+void main()
+{
+  int arr[MAX] [MAX] ={0};
+  int i, j, n=0, start, end;
+  start = end = MAX/2;
+  for(i=0; i<MAX; i++) {
+    for(j=start; j<=end; j++) {
+      n++;
+      arr[i][j] = n;
+    }
+    if(i<MAX/2)
+    {
+        start--;
+        end++;
+    }
+    else
+    {
+        start++;
+        end--;
+    }
+  }
+for(i=0; i<MAX; i++) {
+  for(j=0; j<MAX; j++) {
+    if(arr[i][j]==0)
+    {
+      printf("  ");
+    }
+    else {
+      printf("%3d",arr[i][j]);
+    }
+  }
+  printf("\n");
+}
+}
