de5f93cf98cb765642d86da3093852ff8b229b8f
newjiwon <78717933+newjiwon@users.noreply.github.com>
2026-02-23 14:14:57 +0900

    Create baeyul_test03(inverted_triangle).c

---
+++ baeyul/baeyul_test03(inverted_triangle).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 5
+void main()
+{
+  int arr[MAX] [MAX] ={0};
+  int i, j, n=0;
+  for(i=0; i<MAX; i++) {
+    for(j=0; j<=i; j++) {
+      n++;
+      arr[j][i] = n;
+    }
+  }
+for(i=0; i<MAX; i++) {
+  for(j=0; j<MAX; j++) {
+    if(arr[i][j]==0)
+    {
+      printf("  ");
+    }
+    else {
+      printf("%3d",arr[i][j]);
+    }
+  }
+  printf("\n");
+}
+}
